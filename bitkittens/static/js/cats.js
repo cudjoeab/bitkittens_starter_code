7a224f771214c1fe8f707d49a8bd7e68ac14d775
cudjoeab <cudjoeab@gmail.com>
2019-08-23 16:04:10 -0400

    first ajax request

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    let summonKitties = document.querySelector('.summon-cats');
+
+    summonKitties.addEventListener('click', () => {
+        console.log('meow'); 
+        let request = 
+        axios.get("https://eric-deploy-test.herokuapp.com/cats")
+        .then(function(response){
+            const kitties = response.data.cats
+
+            console.log('the cats have arrived')
+            console.log(kitties)
+
+        })
+    })
 });
