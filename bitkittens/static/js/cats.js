fe3026816fd71dbc611749d3f3ca2f98724f1c86
cudjoeab <cudjoeab@gmail.com>
2019-08-24 16:04:43 -0400

    able to add the cats to their elements from the div, but need to display them!

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     let summonKitties = document.querySelector('.summon-cats');
+    let catBoxes = document.querySelectorAll('.cat-boxes'); 
 
     summonKitties.addEventListener('click', () => {
         console.log('meow'); 
         let request = 
         axios.get("https://eric-deploy-test.herokuapp.com/cats")
         .then(function(response){
-            const kitties = response.data.cats
-
+            const kittiesList = response.data.cats
             console.log('the cats have arrived')
-            console.log(kitties)
+            console.log(kittiesList)
 
+            kittiesList.forEach((box, index) => {
+                box.innerHTML = ""; 
+                const kitty= document.createElement('img');
+                kitty.src = kittiesList[index]['photo'];
+                kitty.alt = `Photo of ${kittiesList[index]['name']}`;
+                // box.appendChild(kitty); 
+             });
+            
         })
-    })
+        .catch((error) => {
+            console.log('-- Received error.');
+            console.log(error);
+        })
+        .then(() => {
+            console.log('-- Request is over.');
+        });
+    });
 });
